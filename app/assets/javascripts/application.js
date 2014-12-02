407d14d1761b31dd46d89f5aca742972244ad0e5
doomsday18 <tettoroberto@hotmail.it>
2014-12-02 23:19:37 +0100

    cancelled application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,30 +25,3 @@
 //= require select2_locale_it
 //= require dynamic_select
 
-function remove_fields(link) {
-    $(link).previous("input[type=hidden]").value = "1";
-    $(link).up(".fields").hide();
-     }
-
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g");
-  $(link).up().insert({
-        before: content.replace(regexp, new_id)
-  });
-}
-
-
-<script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-21225035-2']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
