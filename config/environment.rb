29861d47849e6915054ac7486e8b753f038c3c46
doomsday18 <tettoroberto@hotmail.it>
2014-12-02 19:27:07 +0100

    recaptcha public not deleted

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ENV['RECAPTCHA_PUBLIC_KEY'] = '6Lf3tugSAAAAALfTw-vOA0MLrx0VBFDHjd6Uw-VO'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6Lf3tugSAAAAAPKje4xrv2xWMWSHqwZMBX-W1s8m'
