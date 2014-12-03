6b833c7e2614cf9bd038be9ee96b388d9cb04620
doomsday18 <tettoroberto@hotmail.it>
2014-12-03 23:10:46 +0100

    modified model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,29 @@
 class User < ActiveRecord::Base
+	include ActiveModel::Validations
+      before_create :set_default_role
+      validates_acceptance_of :terms_of_service, :on => :create
+        #enum role: [:admin, :superuser, :simpleuser, :banned]
+      ROLES = %w[admin superuser simpleuser banned]
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  attr_accessible :email, :password, :password_confirmation, :terms_of_service, :role
+  
+  has_many :segnalazione_mineralis, dependent: :destroy
+  has_many :segnalazione_vegetalis, dependent: :destroy
+  has_many :segnalazione_animalis, dependent: :destroy
+  has_many :modulo1s, dependent: :destroy
+  has_many :modulo2s, dependent: :destroy
+
+  private
+  def set_default_role
+      self.role ='simpleuser'
+    end
 end
+
+
+  
