b8ca131b51e79d201b881237d18fb2a42fc2d2e1
doomsday18 <tettoroberto@hotmail.it>
2014-12-04 19:27:10 +0100

    added javascript for captcha

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,15 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-21225035-2']);
+      _gaq.push(['_trackPageview']);
 
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
 
 
 //= require jquery
