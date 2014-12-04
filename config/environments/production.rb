e38f6ea7cd8aa7371d34b45895d5c5af74ca7916
doomsday18 <tettoroberto@hotmail.it>
2014-12-04 12:56:47 +0100

    config action mailer modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {:host => 'cryptic-savannah-8874.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'https://cryptic-savannah-8874.herokuapp.com/'}
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => "127.0.0.1",
