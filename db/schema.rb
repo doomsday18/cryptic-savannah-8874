44f5bf00f704c92847bbc4fc0b5c24f66058fd01
doomsday18 <tettoroberto@hotmail.it>
2014-12-04 17:00:49 +0100

    add confirmable to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204155515) do
+ActiveRecord::Schema.define(version: 20141204155822) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
