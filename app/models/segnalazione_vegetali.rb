677616e67c7b7bd6f43352f105c8922164b2c424
doomsday18 <tettoroberto@hotmail.it>
2014-12-09 12:50:16 +0100

    added all models

--- app/models/segnalazione_vegetali.rb
+++ app/models/segnalazione_vegetali.rb
@@ -1,3 +1,20 @@
 class SegnalazioneVegetali < ActiveRecord::Base
   belongs_to :user
+  #    validates :famiglia_macro, :presence => true
+   #   validates :genere_macro,:presence => true
+    #  validates :foto,:presence => true
+     # validates :famiglia_micro,:presence => true
+    #  validates :genere_micro,:presence => true
+    #  validates :microfoto,:presence => true
+    #  validates :campione,:presence => true
+    #validates :titolo, :presence => true
+    attr_accessible :famiglia_macro, :genere_macro, :foto, :famiglia_micro, :genere_micro, :microfoto, :campione, :Titolo
+
+def self.search(search)
+  if search
+    find(:all, :conditions => ['Titolo LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+end
 end
--- app/models/sistemi_biologici.rb
+++ app/models/sistemi_biologici.rb
@@ -1,3 +1,5 @@
 class SistemiBiologici < ActiveRecord::Base
   belongs_to :modulo2
+
+  attr_accessible :tipologia, :riferimento,:componenti, :note,:referenze,:descrizione,:modulo2_id
 end
